92647f273b89a85d3c70e45bc32a879ec2cd8f06
Aleksandr Dergunov <dergunovs@mail.ru>
2025-09-09 15:53:56 +0300

    chore: initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>{{ msg }}</h1>
+
+    <button type="button" @click="count++">count is {{ count }}</button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+defineProps<{ msg: string }>();
+
+const count = ref(0);
+</script>
